f097db93aaa9d118f277fe92eba38596dbc0e16b
Yomaira Escano <yjescano@gmail.com>
2018-10-19 01:03:52 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -10,7 +10,6 @@ class SearchesController < ApplicationController
         req.params['v'] = '20160201'
         req.params['near'] = params[:zipcode]
         req.params['query'] = 'coffee shop'
-        req.options.timeout = 0
       end
 
       body = JSON.parse(@resp.body)
