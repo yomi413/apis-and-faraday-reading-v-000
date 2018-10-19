d70e1bdd5b54d9f24198991e61294341d1732bb3
Yomaira Escano <yjescano@gmail.com>
2018-10-19 01:02:47 +0000

    automatically backed up by learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,21 +3,22 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
-      req.params['client_id'] = 'CLTC410S5B52YKD2H0FOXWPHK0BQFYIOWLABFYFJEODASV4W'
-      req.params['client_secret'] = 'TPESMTVRRCSAO4W2SKLAABOWAP3IRWJBDFGGCNM34FWLVC0N'
-      req.params['v'] = '20160201'
-      req.params['near'] = params[:zipcode]
-      req.params['query'] = 'coffee shop'
-      req.options.timeout = 0
-    end
+    begin
+      @resp = Faraday.get 'https://api.foursquare.com/v2/venues/search' do |req|
+        req.params['client_id'] = 'CLTC410S5B52YKD2H0FOXWPHK0BQFYIOWLABFYFJEODASV4W'
+        req.params['client_secret'] = 'TPESMTVRRCSAO4W2SKLAABOWAP3IRWJBDFGGCNM34FWLVC0N'
+        req.params['v'] = '20160201'
+        req.params['near'] = params[:zipcode]
+        req.params['query'] = 'coffee shop'
+        req.options.timeout = 0
+      end
 
-    body = JSON.parse(@resp.body)
-    if @resp.success?
-      @venues = body["response"]["venues"]
-    else
-      @error = body["meta"]["errorDetail"]
-    end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @venues = body["response"]["venues"]
+      else
+        @error = body["meta"]["errorDetail"]
+      end
 
   rescue Faraday::ConnectionFailed
     @error = "There was a timeout. Please try again."
